8514c8298a012bc9dd798d1483b2b7589ec89689
Hsa-o <hsijiahao7980@gmail.com>
2026-08-18 20:22:19 -0300

    Tratamento de erro

--- teste.py
+++ teste.py
@@ -1,30 +1,31 @@
 import random
 
-minimo = int(input("Digite o número minimo: "))
-maximo = int(input("Digite o número maximo: "))
 
-numero_sorteado = random.randint(minimo, maximo)
-
+print("Advinhe o numero entre 1 e 100")
+numero_sorteado = random.randint(1, 100)
 tentativa = 0
 
+
 while True:
-    if tentativa >= 5:
+    if tentativa == 5:
         print("voce perdeu")
+        print("numero sorteado foi:", numero_sorteado)
         break
-    
-    numero_digitado = int(input(" "))
-    tentativa += 1
-
-    if numero_digitado > numero_sorteado:
-        print("O número sorteado é menor!")
-
-    elif numero_digitado < numero_sorteado:
-        print("O número sorteado é maior!")
-
-    else:
-        print("Parabéns, o número sorteado foi: ", numero_sorteado)
-        print(f"Voce conseguiu em {tentativa} tentativas")
-        break
-
+    try:
+        numero_digitado = int(input("")) 
+        while numero_digitado < 1 or numero_digitado > 100:
+            print("Entre com numeros validos")
+            numero_digitado = int(input("")) 
+        tentativa += 1
+        if numero_digitado > numero_sorteado:
+            print("O número sorteado é menor!")
+        elif numero_digitado < numero_sorteado:
+            print("O número sorteado é maior!")
+        else:
+            print("Parabéns, o número sorteado foi: ", numero_sorteado)
+            print(f"Voce conseguiu em {tentativa} tentativas")
+            break
+    except ValueError:
+        print("Entre com números válido")
 
 
